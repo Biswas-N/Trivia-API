ba531508db16fe60a53164321fa06debc30b6800
Biswas <biswas.bin2@gmail.com>
2020-05-23 17:01:29 +1000

    Code refactoring based on PEP8

--- backend/models.py
+++ backend/models.py
@@ -8,71 +8,68 @@ database_path = "postgres://{}/{}".format('localhost:5432', database_name)
 
 db = SQLAlchemy()
 
-'''
-setup_db(app)
-    binds a flask application and a SQLAlchemy service
-'''
+
 def setup_db(app, database_path=database_path):
+    """
+      setup_db(app)
+            binds a flask application and a SQLAlchemy service
+    """
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
     db.create_all()
 
-'''
-Question
 
-'''
-class Question(db.Model):  
-  __tablename__ = 'questions'
+# Question model
+class Question(db.Model):
+    __tablename__ = 'questions'
 
-  id = Column(Integer, primary_key=True)
-  question = Column(String)
-  answer = Column(String)
-  category = Column(String)
-  difficulty = Column(Integer)
+    id = Column(Integer, primary_key=True)
+    question = Column(String)
+    answer = Column(String)
+    category = Column(String)
+    difficulty = Column(Integer)
 
-  def __init__(self, question, answer, category, difficulty):
-    self.question = question
-    self.answer = answer
-    self.category = category
-    self.difficulty = difficulty
+    def __init__(self, question, answer, category, difficulty):
+        self.question = question
+        self.answer = answer
+        self.category = category
+        self.difficulty = difficulty
 
-  def insert(self):
-    db.session.add(self)
-    db.session.commit()
-  
-  def update(self):
-    db.session.commit()
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
 
-  def delete(self):
-    db.session.delete(self)
-    db.session.commit()
+    def update(self):
+        db.session.commit()
 
-  def format(self):
-    return {
-      'id': self.id,
-      'question': self.question,
-      'answer': self.answer,
-      'category': self.category,
-      'difficulty': self.difficulty
-    }
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
 
-'''
-Category
+    def format(self):
+        return {
+            'id': self.id,
+            'question': self.question,
+            'answer': self.answer,
+            'category': self.category,
+            'difficulty': self.difficulty
+        }
 
-'''
-class Category(db.Model):  
-  __tablename__ = 'categories'
 
-  id = Column(Integer, primary_key=True)
-  type = Column(String)
+# Category model
+class Category(db.Model):
+    __tablename__ = 'categories'
 
-  def __init__(self, type):
-    self.type = type
+    id = Column(Integer, primary_key=True)
+    type = Column(String)
 
-  def format(self):
-    return {
-      'id': self.id,
-      'type': self.type
-    }
+    def __init__(self, type):
+        self.type = type
+
+    def format(self):
+        return {
+            'id': self.id,
+            'type': self.type
+        }
