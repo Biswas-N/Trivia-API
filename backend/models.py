2556be48c394e0e6ca068e5059be54188d1941b8
Biswas <biswas.bin2@gmail.com>
2020-05-24 22:09:46 +1000

    Added model relationship between Question and Category

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import relationship
 from flask_sqlalchemy import SQLAlchemy
 
 database_name = "trivia"
@@ -28,13 +29,12 @@ class Question(db.Model):
     id = Column(Integer, primary_key=True)
     question = Column(String)
     answer = Column(String)
-    category = Column(String)
+    category_id = Column(Integer, ForeignKey('categories.id'))
     difficulty = Column(Integer)
 
-    def __init__(self, question, answer, category, difficulty):
+    def __init__(self, question, answer, difficulty):
         self.question = question
         self.answer = answer
-        self.category = category
         self.difficulty = difficulty
 
     def insert(self):
@@ -53,7 +53,7 @@ class Question(db.Model):
             'id': self.id,
             'question': self.question,
             'answer': self.answer,
-            'category': self.category,
+            'category': self.category.id,
             'difficulty': self.difficulty
         }
 
@@ -64,6 +64,7 @@ class Category(db.Model):
 
     id = Column(Integer, primary_key=True)
     type = Column(String)
+    questions = relationship('Question', backref='category', lazy=True)
 
     def __init__(self, type):
         self.type = type
